2ae0c8d64a64fc0211668d597bc63459142dcf0d
Wilian Ansanello <wiliansanello@outlook.com>
2024-03-19 22:02:30 -0300

    first commit

---
+++ health_history/src/main/java/com/wiliansanello/health_history/HealthHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.wiliansanello.health_history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthHistoryApplication.class, args);
+	}
+
+}
---
+++ health_history/src/test/java/com/wiliansanello/health_history/HealthHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiliansanello.health_history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
